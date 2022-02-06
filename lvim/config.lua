c6297f413487b62c2dfac324fa4433adc547eb74
Derek Wan <d.wan@icloud.com>
2022-02-06 20:11:13 +0800

    more

--- lvim/config.lua
+++ lvim/config.lua
@@ -92,157 +92,153 @@ local which_key = lvim.builtin.which_key.mappings
 
 which_key["a"] = {
 	name = "...",
-	c = { "<Esc><Cmd>Telescope autocommands<CR>", "Autocommands" },
+	c = { "<Cmd>Telescope autocommands<CR>", "Autocommands" },
 }
 which_key["b"] = {
 	name = "...",
 	-- default
-	b = { "<Esc><Cmd>Telescope buffers<CR>", "Buffers" },
+	b = { "<Cmd>Telescope buffers<CR>", "Buffers" },
 	-- others
-	d = { "<Esc><Cmd>BufferDelete<CR>", "Delete" },
-	f = { "<Esc><Cmd>Telescope current_buffer_fuzzy_find<CR>", "Fuzzy find" },
+	d = { "<Cmd>BufferDelete<CR>", "Delete" },
+	f = { "<Cmd>Telescope current_buffer_fuzzy_find<CR>", "Fuzzy find" },
 }
 which_key["c"] = {
 	name = "...",
 	-- default
-	c = { "<Esc><Cmd>Telescope commands<CR>", "Commands" },
+	c = { "<Cmd>Telescope commands<CR>", "Commands" },
 	-- others
-	a = { "<Esc><Cmd>Telescope lsp_code_actions<CR>", "Code actions" },
-	h = { "<Esc><Cmd>Telescope command_history<CR>", "Command history" },
+	a = { "<Cmd>Telescope lsp_code_actions<CR>", "Code actions" },
+	h = { "<Cmd>Telescope command_history<CR>", "Command history" },
 }
 which_key["d"] = {
 	name = "...",
 	-- diagnostics
-	d = { "<Esc><Cmd>Telescope diagnostics<CR>", "Diagostics" },
+	d = { "<Cmd>Telescope diagnostics<CR>", "Diagostics" },
 	-- others
 	k = {
-		"<Esc><Cmd>lua vim.diagnostic.goto_prev()<CR>",
+		"<Cmd>lua vim.diagnostic.goto_prev()<CR>",
 		"Previous diagnostic",
 	},
-	f = { "<Esc><Cmd>Telescope lsp_definitions<CR>", "Definitions" },
+	f = { "<Cmd>Telescope lsp_definitions<CR>", "Definitions" },
 	---- diagnostics
-	j = { "<Esc><Cmd>lua vim.diagnostic.goto_next()<CR>", "Next diagostic" },
-	s = { "<Esc><Cmd>Telescope lsp_document_symbols<CR>", "Document symbols" },
+	j = { "<Cmd>lua vim.diagnostic.goto_next()<CR>", "Next diagostic" },
+	s = { "<Cmd>Telescope lsp_document_symbols<CR>", "Document symbols" },
 }
 which_key["e"] = {
 	name = "...",
-	m = { "<Esc><Cmd>EnMasse<CR>", "EnMasse" },
+	m = { "<Cmd>EnMasse<CR>", "EnMasse" },
 }
 which_key["f"] = {
 	name = "...",
 	-- default
-	f = { "<Esc><Cmd>Telescope find_files<CR>", "Find files" },
+	f = { "<Cmd>Telescope find_files<CR>", "Find files" },
 }
 which_key["g"] = {
 	name = "...",
-	b = { "<Esc><Cmd>Telescope git_bcommits<CR>", "Git buffer commits" },
-	c = { "<Esc><Cmd>Telescope git_commits<CR>", "Git commits" },
-	f = { "<Esc><Cmd>Telescope git_files<CR>", "Git files" },
-	r = { "<Esc><Cmd>Telescope grep_string<CR>", "Grep string" },
-	s = { "<Esc><Cmd>Telescope git_status<CR>", "Git status" },
+	b = { "<Cmd>Telescope git_bcommits<CR>", "Git buffer commits" },
+	c = { "<Cmd>Telescope git_commits<CR>", "Git commits" },
+	f = { "<Cmd>Telescope git_files<CR>", "Git files" },
+	r = { "<Cmd>Telescope grep_string<CR>", "Grep string" },
+	s = { "<Cmd>Telescope git_status<CR>", "Git status" },
 }
 which_key["h"] = {
 	name = "...",
-	t = { "<Esc><Cmd>Telescope help_tags<CR>", "Help tags" },
+	t = { "<Cmd>Telescope help_tags<CR>", "Help tags" },
 }
 which_key["i"] = {
 	name = "...",
-	m = { "<Esc><Cmd>Telescope lsp_implementations<CR>", "Implementations" },
+	m = { "<Cmd>Telescope lsp_implementations<CR>", "Implementations" },
 }
-which_key["j"] = { "<Esc><Cmd>Telescope jumplist<CR>", "Jump list" }
+which_key["j"] = { "<Cmd>Telescope jumplist<CR>", "Jump list" }
 which_key["k"] = {
 	name = "...",
-	m = { "<Esc><Cmd>Telescope keymaps<CR>", "Key maps" },
+	m = { "<Cmd>Telescope keymaps<CR>", "Key maps" },
 }
 which_key["l"] = {
 	name = "...",
 	-- trouble
-	d = { "<Esc><Cmd>Trouble document_diagnostics<CR>", "Diagnostics" },
-	f = { "<Esc><Cmd>Trouble lsp_definitions<CR>", "Definitions" },
-	r = { "<Esc><Cmd>Trouble lsp_references<CR>", "References" },
-	q = { "<Esc><Cmd>Trouble quickfix<CR>", "Quick Fix" },
+	d = { "<Cmd>Trouble document_diagnostics<CR>", "Diagnostics" },
+	f = { "<Cmd>Trouble lsp_definitions<CR>", "Definitions" },
+	r = { "<Cmd>Trouble lsp_references<CR>", "References" },
+	q = { "<Cmd>Trouble quickfix<CR>", "Quick Fix" },
 	w = {
-		"<Esc><Cmd>Trouble workspace_diagnostics<CR>",
+		"<Cmd>Trouble workspace_diagnostics<CR>",
 		"Workspace diagnostics",
 	},
 	-- others
-	g = { "<Esc><Cmd>Telescope live_grep<CR>", "Live grep" },
-	l = { "<Esc><Cmd>Telescope loclist<CR>", "Location list" },
+	g = { "<Cmd>Telescope live_grep<CR>", "Live grep" },
+	l = { "<Cmd>Telescope loclist<CR>", "Location list" },
 }
-which_key["m"] = { "<Esc><Cmd>Telescope marks<CR>", "Marks" }
+which_key["m"] = { "<Cmd>Telescope marks<CR>", "Marks" }
 which_key["o"] = {
 	name = "...",
-	f = { "<Esc><Cmd>Telescope oldfiles<CR>", "Old files" },
+	f = { "<Cmd>Telescope oldfiles<CR>", "Old files" },
 }
-which_key["q"] = { "<Esc><Cmd>Telescope quickfix<CR>", "Quickfix" }
+which_key["q"] = { "<Cmd>Telescope quickfix<CR>", "Quickfix" }
 which_key["r"] = {
 	name = "...",
 	-- LSP
 	c = {
-		"<Esc><Cmd>Telescope lsp_range_code_actions<CR>",
+		"<Cmd>Telescope lsp_range_code_actions<CR>",
 		"Range code actions",
 	},
-	f = { "<Esc><Cmd>Telescope lsp_references<CR>", "References" },
-	g = { "<Esc><Cmd>Telescope registers<CR>", "Registers" },
-	n = { "<Esc><Cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
+	f = { "<Cmd>Telescope lsp_references<CR>", "References" },
+	g = { "<Cmd>Telescope registers<CR>", "Registers" },
+	n = { "<Cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
 }
 which_key["s"] = {
 	name = "...",
 	-- default
-	s = { "<Esc><Cmd>Telescope symbols<CR>", "Symbols" },
+	s = { "<Cmd>Telescope symbols<CR>", "Symbols" },
 	-- Spectre
-	o = { "<Esc><Cmd>lua require('spectre').open()<CR>", "Spectre" },
+	o = { "<Cmd>lua require('spectre').open()<CR>", "Spectre" },
 	f = {
-		"<Esc><Cmd>lua require('spectre').open_file_search()<CR>",
+		"<Cmd>lua require('spectre').open_file_search()<CR>",
 		"Spectre: File",
 	},
-	v = {
-		"<Esc><Cmd>lua require('spectre').open_visual({select_word=true})<CR>",
-		"Spectre: Visual",
-	},
 	-- others
-	h = { "<Esc><Cmd>Telescope search_history<CR>", "Search history" },
-	p = { "<Esc><Cmd>Telescope spell_suggest<CR>", "Spell suggest" },
+	h = { "<Cmd>Telescope search_history<CR>", "Search history" },
+	p = { "<Cmd>Telescope spell_suggest<CR>", "Spell suggest" },
 }
 which_key["t"] = {
 	name = "...",
 	-- default
-	t = { "<Esc><Cmd>NvimTreeToggle<CR>", "NvimTree" },
+	t = { "<Cmd>NvimTreeToggle<CR>", "NvimTree" },
 	-- others
-	d = { "<Esc><Cmd>Telescope lsp_type_definitions<CR>", "Type definitions" },
+	d = { "<Cmd>Telescope lsp_type_definitions<CR>", "Type definitions" },
 }
 which_key["v"] = {
 	name = "...",
-	o = { "<Esc><Cmd>Telescope vim_options<CR>", "Vim options" },
+	o = { "<Cmd>Telescope vim_options<CR>", "Vim options" },
 }
 which_key["w"] = {
 	name = "...",
 	-- default
-	w = { "<Esc><Cmd>lua require('nvim-window').pick()<CR>", "Windows" },
+	w = { "<Cmd>lua require('nvim-window').pick()<CR>", "Windows" },
 	-- windows
-	h = {
-		"<Esc><Cmd>set nosplitright<CR><Esc><Cmd>vsplit<CR>",
-		"Window: left",
-	},
-	j = { "<Esc><Cmd>set splitbelow<CR><Esc><Cmd>split<CR>", "Window: down" },
-	k = { "<Esc><Cmd>set nosplitbelow<CR><Esc><Cmd>split<CR>", "Window: up" },
-	l = {
-		"<Esc><Cmd>set splitright<CR><Esc><Cmd>vsplit<CR>",
-		"Window: right",
-	},
+	h = { "<Cmd>set nosplitright<CR><Cmd>vsplit<CR>", "Window: left" },
+	j = { "<Cmd>set splitbelow<CR><Cmd>split<CR>", "Window: down" },
+	k = { "<Cmd>set nosplitbelow<CR><Cmd>split<CR>", "Window: up" },
+	l = { "<Cmd>set splitright<CR><Cmd>vsplit<CR>", "Window: right" },
 	-- LSP
-	s = {
-		"<Esc><Cmd>Telescope lsp_workspace_symbols<CR>",
-		"Workspace symbols",
-	},
+	s = { "<Cmd>Telescope lsp_workspace_symbols<CR>", "Workspace symbols" },
 }
 
 local which_key_visual = lvim.builtin.which_key.vmappings
 
-which_key_visual["c"] = { "<Esc><Cmd>Telescope commands<CR>", "Commands" }
+which_key_visual["c"] = { ":Telescope commands<CR>", "Commands" }
+which_key_visual["n"] = { r = { ":NR<CR>", "NrrwRgn" } }
 which_key_visual["s"] = {
-	"<Esc><Cmd>lua require('spectre').open_visual({select_word=true})<CR>",
+	":lua require('spectre').open_visual({select_word=true})<CR>",
 	"Spectre",
+}
+which_key_visual["v"] = {
+	name = "...",
+	-- default
+	v = { ":VSSplit<CR>", "VSSplit" },
+	-- others
+	j = { ":VSSplitBelow<CR>", "VSSplitBelow" },
+	k = { ":VSSplitAbove<CR>", "VSSplitAbove" },
 }
 
 --------------------------------------------------------------------------------
@@ -316,6 +312,9 @@ lvim.plugins = {
 
 	-- editing: quoting and parenthesizing
 	{ "tpope/vim-surround" },
+
+	-- editing: visual splits
+	{ "wellle/visual-split.vim" },
 
 	-- git: conflict markers
 	{ "rhysd/conflict-marker.vim" },
