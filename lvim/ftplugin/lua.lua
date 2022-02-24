2c95d3de80ee5774ac759f0e255c0ffe99c7a580
Derek Wan <d.wan@icloud.com>
2022-02-25 08:40:11 +0900

    Remove ftplugin

--- lvim/ftplugin/lua.lua
+++
@@ -1,5 +0,0 @@
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup({ { exe = "stylua" } })
-
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({ { exe = "luacheck" } })
--- lvim/ftplugin/sh.lua
+++
@@ -1,5 +0,0 @@
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup({ { exe = "shfmt" } })
-
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({ { exe = "shellcheck" } })
