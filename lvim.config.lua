4109f4df9fe99842572c1374390f9f522058cadf
Derek Wan <d.wan@icloud.com>
2022-04-02 10:48:24 +0900

    Add packersync

--- lvim.config.lua
+++ lvim.config.lua
@@ -206,7 +206,13 @@ which_key["o"] = {
 	name = "...",
 	f = { "<Cmd>Telescope oldfiles<CR>", "Old files" },
 }
-which_key["p"] = { "<Cmd>Telescope projects<CR>", "Projects" }
+which_key["p"] = {
+	name = "...",
+	-- packer
+	s = { "<Cmd>PackerSync<CR>", "Packer sync" },
+	-- projects
+	r = { "<Cmd>Telescope projects<CR>", "Projects" },
+}
 which_key["q"] = { "<Cmd>Telescope quickfix<CR>", "Quickfix" }
 which_key["r"] = {
 	name = "...",
