c1f71c73ef69bc726f6a34f5e6c81004e2148b20
Derek Wan <d.wan@icloud.com>
2022-04-16 17:52:37 +0800

    Add nvim-treesitter-context

--- lvim.config.lua
+++ lvim.config.lua
@@ -593,6 +593,29 @@ lvim.plugins = {
 	-- viewing: absolute line numbers in inactive windows
 	{ "jeffkreeftmeijer/vim-numbertoggle" },
 
+	-- viewing: context
+	{
+		"romgrk/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+				throttle = true, -- Throttles plugin updates (may improve performance)
+				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+				patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+					-- For all filetypes
+					-- Note that setting an entry here replaces all other patterns for this entry.
+					-- By setting the 'default' entry below, you can control which nodes you want to
+					-- appear in the context window.
+					default = {
+						"class",
+						"function",
+						"method",
+					},
+				},
+			})
+		end,
+	},
+
 	-- viewing: indent guides
 	{
 		"lukas-reineke/indent-blankline.nvim",
