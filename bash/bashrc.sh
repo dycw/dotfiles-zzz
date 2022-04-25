ecf7ce548f1c63d5cb6db2f806c3a58d63f7efad
Derek Wan <d.wan@icloud.com>
2022-04-26 07:15:38 +0800

    Bump version: 0.2.7 → 0.2.8

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -333,8 +333,7 @@ alias pipconf='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/pip/pip.conf"'
 alias pypirc='$EDITOR "$HOME/.pypirc"'
 
 # pip-tools
-alias psdev='pip install pip-tools && pip-sync requirements-dev.txt'
-alias psreq='pip install pip-tools && pip-sync requirements.txt'
+alias psreq='pip install pip-tools && pip-sync requirements.txt requirements-dev.txt'
 
 # poetry
 _BIN="$HOME/.poetry/bin"
@@ -350,8 +349,9 @@ fi
 alias pguser='sudo -i -u postgres'
 
 # pre-commit
-alias pca='pre-commit run -a'
-alias pcav='pre-commit run -av'
+alias pcr='pre-commit run'
+alias pcra='pre-commit run -a'
+alias pcrav='pre-commit run -av'
 alias pci='pip install pre-commit && pre-commit install'
 alias pcau='pre-commit autoupdate'
 alias pcaua='pre-commit autoupdate && pre-commit run -a'
