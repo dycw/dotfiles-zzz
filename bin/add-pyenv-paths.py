c05eebb245fb74d498d6a5c75b35c79b20e4b837
Derek Wan <d.wan@icloud.com>
2022-05-08 20:22:50 +0800

    Expand `lvim` snippets (#65)

---
+++ bin/add-pyenv-paths.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from subprocess import check_output  # noqa: S404
+from sys import stdout
+
+
+def main() -> None:
+    versions = check_output(  # noqa: S607
+        "pyenv versions --bare --skip-aliases",
+        shell=True,  # noqa: S602
+        text=True,
+    ).splitlines()
+    for version in versions:
+        stdout.write(_get_path(version) + "\n")
+
+
+def _get_path(version: str) -> str:
+    return check_output(
+        f"PYENV_VERSION={version} pyenv which python",
+        shell=True,  # noqa: S602
+        text=True,
+    ).rstrip("\n")
+
+
+if __name__ == "__main__":
+    main()
