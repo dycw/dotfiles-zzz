34c245d0ad186b4ead4852174a178aa24b24c3db
Derek Wan <d.wan@icloud.com>
2022-09-24 15:15:39 +0800

    Redo nvim-lastplace

--- lvim/config.lua
+++ lvim/config.lua
@@ -562,8 +562,18 @@ lvim.plugins = {
 	-- navigation: re-open files at last edit position
 	{
 		"ethanholz/nvim-lastplace",
+		event = "BufRead",
 		config = function()
-			require("nvim-lastplace").setup()
+			require("nvim-lastplace").setup({
+				lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+				lastplace_ignore_filetype = {
+					"gitcommit",
+					"gitrebase",
+					"svn",
+					"hgcommit",
+				},
+				lastplace_open_folds = true,
+			})
 		end,
 	},
 
