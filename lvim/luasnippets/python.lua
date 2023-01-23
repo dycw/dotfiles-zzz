b02ddfa8166bc6df2d4de3a60a6bcc28d650e26f
Derek Wan <d.wan@icloud.com>
2023-01-23 16:39:53 +0800

    Add `luasnippets` (#139)

---
+++ lvim/luasnippets/python.lua
@@ -0,0 +1,11 @@
+return {
+	s("bp", { t("breakpoint()") }),
+	s("fbearbeartype", { t("from beartype import beartype") }),
+	s("fdatadataclass", { t("from dataclasses import dataclass") }),
+	s("fhypdata", { t("from hypothesis import data") }),
+	s("fhypgiven", { t("from hypothesis import given") }),
+	s("fhypreproduce", { t("from hypothesis import reproduce_failure") }),
+	s("imdt", { t("import datetime as dt") }),
+	s("imnp", { t("import numpy as np") }),
+	s("impd", { t("import pandas as pd") }),
+}
