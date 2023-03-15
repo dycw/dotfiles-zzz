dc5027c452b42f173afe44bb9991d3fe4b53bad4
Derek Wan <d.wan@icloud.com>
2023-03-15 22:17:58 +0800

    Import `dataclasses` if `attrs` not found, only

--- ipython/startup/import_dataclasses.py
+++ ipython/startup/import_dataclasses.py
@@ -1,7 +1,13 @@
 import dataclasses  # noqa: F401
-from dataclasses import asdict  # noqa: F401
-from dataclasses import astuple  # noqa: F401
-from dataclasses import dataclass  # noqa: F401
-from dataclasses import field  # noqa: F401
-from dataclasses import fields  # noqa: F401
 from dataclasses import replace  # noqa: F401
+
+from dataclasses import dataclass  # noqa: F401
+
+
+try:
+    import attrs  # noqa: F401
+except ModuleNotFoundError:
+    from dataclasses import asdict  # noqa: F401
+    from dataclasses import astuple  # noqa: F401
+    from dataclasses import field  # noqa: F401
+    from dataclasses import fields  # noqa: F401
