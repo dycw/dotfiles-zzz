575afc7b9b347edde0f3662591bdd29556c49453
Derek Wan <d.wan@icloud.com>
2023-04-30 14:58:50 +0800

    Guard `dropbox` env on various machines

--- dropbox/env.sh
+++ dropbox/env.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-_dir="$HOME/Dropbox"
-if [ -d "$_dir" ]; then
-	export PATH_DROPBOX="$_dir"
-fi
+case "$(uname -s)" in
+Darwin*)
+	_dir="$HOME/Dropbox"
+	if [ -d "$_dir" ]; then
+		export PATH_DROPBOX="$_dir"
+	fi
+	;;
+Linux*) ;;
+*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
+esac
