dfdbb4152b7ed7e396a86921ba82252886fba378
Derek Wan <d.wan@icloud.com>
2023-04-30 11:29:57 +0800

    Remove skipping messages, add all Python versions (#316)
    * Remove "skipping" messages
    
    * Also add installation of Python

--- brew/config.sh
+++ brew/config.sh
@@ -2,6 +2,6 @@
 
 case "$(uname -s)" in
 Darwin*) eval "$(/opt/homebrew/bin/brew shellenv)" ;;
-Linux*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Skipping for Linux..." ;;
+Linux*) ;;
 *) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
 esac
