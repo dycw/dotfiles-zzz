7f5095c9b4064bf6147669af13c210e9d1fcfd24
Derek Wan <d.wan@icloud.com>
2023-04-30 14:22:17 +0800

    Prefix some vars with _

--- haskell/config.sh
+++ haskell/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-file="$HOME/.ghcup/env"
-if [ -f "$file" ]; then
+_file="$HOME/.ghcup/env"
+if [ -f "$_file" ]; then
 	# shellcheck source=/dev/null
-	source "$file"
+	source "$_file"
 fi
--- nvm/autocomplete.sh
+++ nvm/autocomplete.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-file="${NVM_DIR:-${XDG_CONFIG_HOME:-$HOME/.config}/nvm}/bash_completion"
-if [ -f "$file" ]; then
-	# shellcheck source=/dev/null
-	source "$file"
+if [ -n "${BASH_VERSION+x}" ]; then
+	_file="${NVM_DIR:-${XDG_CONFIG_HOME:-$HOME/.config}/nvm}/bash_completion"
+	if [ -f "$_file" ]; then
+		# shellcheck source=/dev/null
+		source "$_file"
+	fi
 fi
