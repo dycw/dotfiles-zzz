3e29eb9a20ff2af52e163c4f988b6322f3f3de69
Derek Wan <d.wan@icloud.com>
2023-05-01 15:59:18 +0800

    Fix 1password autocomplete

--- 1password/autocomplete.sh
+++ 1password/autocomplete.sh
@@ -3,4 +3,7 @@
 if [ -n "${BASH_VERSION+x}" ]; then
 	# shellcheck source=/dev/null
 	source <(op completion bash)
+elif [ -n "${ZSH_VERSION+x}" ]; then
+	eval "$(op completion zsh)"
+	compdef _op op
 fi
