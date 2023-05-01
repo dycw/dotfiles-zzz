eee7ee7309fc6eb8dc4f41a903970e5ce6afaf4e
Derek Wan <d.wan@icloud.com>
2023-05-01 10:14:21 +0800

    more

--- exa/install.sh
+++ exa/install.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking for exa..."
-
-if ! [ -x "$(command -v exa)" ]; then
-	echo "$(date '+%Y-%m-%d %H:%M:%S'): Installing exa..."
-	case "$(uname -s)" in
-	Darwin*)
-		# shellcheck source=/dev/null
-		source "$(git rev-parse --show-toplevel)/brew/install-package.sh" exa exa
-		;;
-	Linux*) sudo apt -y install exa ;;
-	*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
-	esac
-fi
+# shellcheck source=/dev/null
+source "$(git rev-parse --show-toplevel)/brew/install-package.sh" dust dust
