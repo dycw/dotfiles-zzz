2644dc83727640b6a874e6777f675017c69520e9
Derek Wan <d.wan@icloud.com>
2023-05-01 10:14:36 +0800

    more

--- exa/install.sh
+++ exa/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # shellcheck source=/dev/null
-source "$(git rev-parse --show-toplevel)/brew/install-package.sh" dust dust
+source "$(git rev-parse --show-toplevel)/brew/install-package.sh" exa exa
