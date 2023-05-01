e884b75dbe79d50cec611ef8e6c9f866d64cc597
Derek Wan <d.wan@icloud.com>
2023-05-01 18:31:13 +0800

    Fix `certbot` installer (#339)

--- dropbox/install.sh
+++ dropbox/install.sh
@@ -2,9 +2,7 @@
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Running dropbox/install.sh..."
 
-if
-	[[ "$(uname -s)" =~ Darwin* ]]
-then
+if [[ "$(uname -s)" =~ Darwin* ]]; then
 	# shellcheck source=/dev/null
 	source "$(git rev-parse --show-toplevel)/brew/install.sh"
 	if ! grep -Fxq dropbox <<<"$(brew list -1)"; then
