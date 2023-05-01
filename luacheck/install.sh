388a32686442b428f6e8e5d2a8f6b1e304020f32
Derek Wan <d.wan@icloud.com>
2023-05-01 14:14:55 +0800

    luacheck

--- luacheck/install.sh
+++ luacheck/install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# shellcheck source=/dev/null
+source "$(git rev-parse --show-toplevel)/brew/install-package.sh" \
+	luacheck luacheck
+
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Symlinking for luacheck..."
 # shellcheck source=/dev/null
 source "$(git rev-parse --show-toplevel)/installers/symlink.sh" \
