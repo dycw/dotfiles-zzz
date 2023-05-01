5cbd38863a7f5201d195d89d82409d30dd789a0c
Derek Wan <d.wan@icloud.com>
2023-05-01 10:30:43 +0800

    more

--- fd/install.sh
+++ fd/install.sh
@@ -1,27 +1,4 @@
 #!/usr/bin/env bash
 
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking for fd..."
-
-_root="$(git rev-parse --show-toplevel)"
-if ! [ -x "$(command -v fd)" ]; then
-	echo "$(date '+%Y-%m-%d %H:%M:%S'): Installing fd..."
-	case "$(uname -s)" in
-	Darwin*)
-		# shellcheck source=/dev/null
-		source "$_root/brew/install-package.sh" fd fd
-		;;
-	Linux*)
-		sudo apt -y install fd-find
-		_bin_dir="$HOME/.local/bin/"
-		mkdir -p "$_bin_dir"
-		ln -s "$(which fdfind)" "$_bin_dir/fd"
-		;;
-	*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
-	esac
-fi
-
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Symlinking for fd..."
 # shellcheck source=/dev/null
-source "$_root/installers/symlink.sh" \
-	"$HOME/dotfiles/fd/ignore" \
-	"${XDG_CONFIG_HOME:-$HOME/.config}/fd/ignore"
+source "$(git rev-parse --show-toplevel)/brew/install-package.sh" fd fd
