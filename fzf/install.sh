3562479fecc4cb584b9c2d5c446e215f91f18cc9
Derek Wan <d.wan@icloud.com>
2023-05-01 13:17:09 +0800

    more

--- fzf/install.sh
+++ fzf/install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# shellcheck source=/dev/null
-source "$(git rev-parse --show-toplevel)/brew/install-package.sh" fzf fzf
-
 if ! [ -x "$(command -v fzf)" ]; then
 	# shellcheck source=/dev/null
+	source "$(git rev-parse --show-toplevel)/brew/install-package.sh" fzf fzf
 
-	"root/install" --xdg --key-bindings --completion --no-update-rc --no-fish
+	"$(brew --prefix)/opt/fzf/install" \
+		--xdg --key-bindings --completion --no-update-rc --no-fish
 fi
