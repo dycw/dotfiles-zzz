cd115d0aeb30ebaee322de61cc07814716e62b74
Derek Wan <d.wan@icloud.com>
2023-05-01 14:03:06 +0800

    bash comp

--- bash/config.sh
+++ bash/config.sh
@@ -14,4 +14,12 @@ if [ -n "${BASH_VERSION+x}" ]; then
 	shopt -s nocasematch
 	shopt -s shift_verbose
 	shopt -s xpg_echo
+
+	if [ -x "$(command -v brew)" ]; then
+		_dir="$(brew --prefix)/etc/bash_completion"
+		if [ -f "$_dir" ]; then
+			# shellcheck source=/dev/null
+			source "$_dir"
+		fi
+	fi
 fi
