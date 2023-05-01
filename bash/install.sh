3f9f20171ba6c6cc67944c70ca42edc68a3ab9df
Derek Wan <d.wan@icloud.com>
2023-05-01 15:51:55 +0800

    Add `topgrade` (#332)
    * Add `topgrade`
    
    * Extend `bash/zsh`

--- bash/install.sh
+++ bash/install.sh
@@ -2,21 +2,19 @@
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Running bash/install.sh..."
 
-if [ -n "${BASH_VERSION+x}" ]; then
-	_root="$(git rev-parse --show-toplevel)"
-	# shellcheck source=/dev/null
-	source "$_root/brew/install.sh"
-	if ! grep -Fxq bash-completion <<<"$(brew list -1)"; then
-		brew install bash-completion
-	fi
-
-	# folders
-	mkdir -p "$HOME/.cache/bash"
-
-	# symlinks
-	for _name in bash_profile bashrc; do
-		# shellcheck source=/dev/null
-		source "$_root/installers/symlink.sh" \
-			"$HOME/dotfiles/bash/$_name" "$HOME/.$_name"
-	done
+_root="$(git rev-parse --show-toplevel)"
+# shellcheck source=/dev/null
+source "$_root/brew/install.sh"
+if ! grep -Fxq bash-completion <<<"$(brew list -1)"; then
+	brew install bash-completion
 fi
+
+# folders
+mkdir -p "$HOME/.cache/bash"
+
+# symlinks
+for _name in bash_profile bashrc; do
+	# shellcheck source=/dev/null
+	source "$_root/installers/symlink.sh" \
+		"$HOME/dotfiles/bash/$_name" "$HOME/.$_name"
+done
--- zsh/install.sh
+++ zsh/install.sh
@@ -2,21 +2,19 @@
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Running zsh/install.sh..."
 
-if [ -n "${ZSH_VERSION+x}" ]; then
-	_root="$(git rev-parse --show-toplevel)"
-	# shellcheck source=/dev/null
-	source "$_root/brew/install.sh"
-	if ! grep -Fxq zsh-completions <<<"$(brew list -1)"; then
-		brew install zsh-completions
-	fi
-
-	# folders
-	mkdir -p "$HOME/.cache/zsh"
-
-	# symlinks
-	for _name in zprofile zshrc; do
-		# shellcheck source=/dev/null
-		source "$_root/installers/symlink.sh" \
-			"$HOME/dotfiles/zsh/$_name" "$HOME/.$_name"
-	done
+_root="$(git rev-parse --show-toplevel)"
+# shellcheck source=/dev/null
+source "$_root/brew/install.sh"
+if ! grep -Fxq zsh-completions <<<"$(brew list -1)"; then
+	brew install zsh-completions
 fi
+
+# folders
+mkdir -p "$HOME/.cache/zsh"
+
+# symlinks
+for _name in zprofile zshrc; do
+	# shellcheck source=/dev/null
+	source "$_root/installers/symlink.sh" \
+		"$HOME/dotfiles/zsh/$_name" "$HOME/.$_name"
+done
