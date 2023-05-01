e4fd9912402ed88b6716fdb06af150d7874cf596
Derek Wan <d.wan@icloud.com>
2023-05-01 14:21:48 +0800

    multipass

--- multipass/install.sh
+++ multipass/install.sh
@@ -5,7 +5,11 @@ echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking for multipass..."
 if ! [ -x "$(command -v multipass)" ]; then
 	echo "$(date '+%Y-%m-%d %H:%M:%S'): Installing multipass..."
 	case "$(uname -s)" in
-	Darwin*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Skipping for Mac..." ;;
+	Darwin*)
+		# shellcheck source=/dev/null
+		source "$(git rev-parse --show-toplevel)/brew/install-package.sh" \
+			--cask multipass multipass
+		;;
 	Linux*) sudo snap install multipass ;;
 	*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
 	esac
