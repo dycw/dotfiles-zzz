4c71bbc77fb6e1747c24957c2ac044c11b54bf3f
Derek Wan <d.wan@icloud.com>
2023-05-01 14:08:08 +0800

    gitweb

--- gitweb/install.sh
+++ gitweb/install.sh
@@ -1,29 +1,11 @@
 #!/usr/bin/env bash
 
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking for gitweb..."
-
 if ! [ -x "$(command -v gitweb)" ]; then
-	echo "$(date '+%Y-%m-%d %H:%M:%S'): Installing gitweb..."
-	_root="$(git rev-parse --show-toplevel)"
-	case "$(uname -s)" in
-	Darwin*)
-		# shellcheck source=/dev/null
-		source "$_root/brew/install-package.sh" gitweb yoannfleurydev/gitweb/gitweb
-		;;
-	Linux*)
-		_temp_dir="$(mktemp -d -t sd-"$(date +%Y%m%d-%H%M%S)"-XXXXXXXX)"
-		# shellcheck source=/dev/null
-		source "$_root/installers/download-from-github.sh" \
-			"$_temp_dir" yoannfleurydev gitweb linux
-		(
-			cd "$_temp_dir" || exit
-			mv ./*gitweb* gitweb
-			chmod u+x gitweb
-			_bin_dir="$HOME/.local/bin/"
-			mkdir -p "$_bin_dir"
-			mv gitweb "$_bin_dir/"
-		)
-		rm -rf "$_temp_dir"
+	# shellcheck source=/dev/null
+	source "$(git rev-parse --show-toplevel)/brew/install-package.sh" \
+		gitweb yoannfleurydev/gitweb/gitweb
+
+	if [[ "$(uname -s)" =~ Linux* ]]; then
 		# additional error:
 		# ./gitweb: error while loading shared libraries: libssl.so.1.1: cannot
 		# open shared object file: No such file or directory
@@ -32,7 +14,5 @@ if ! [ -x "$(command -v gitweb)" ]; then
 			sudo tee /etc/apt/sources.list.d/focal-security.list
 		sudo apt -y update
 		sudo apt -y install libssl1.1
-		;;
-	*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
-	esac
+	fi
 fi
