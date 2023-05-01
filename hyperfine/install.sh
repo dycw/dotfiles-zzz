1f9252f68a6984f7a3a00af7dc2d05737c56ac61
Derek Wan <d.wan@icloud.com>
2023-05-01 14:10:34 +0800

    hyperfine

--- hyperfine/install.sh
+++ hyperfine/install.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking for hyperfine..."
-
-_root="$(git rev-parse --show-toplevel)"
-if ! [ -x "$(command -v hyperfine)" ]; then
-	echo "$(date '+%Y-%m-%d %H:%M:%S'): Installing hyperfine..."
-	case "$(uname -s)" in
-	Darwin*)
-		# shellcheck source=/dev/null
-		source "$_root/brew/install-package.sh" hyperfine hyperfine
-		;;
-	Linux*) echo "$(date '+%Y-%m-%d %H:%M:%S'): NOT IMPLEMENTED YET..." ;;
-	*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
-	esac
-fi
+# shellcheck source=/dev/null
+source "$(git rev-parse --show-toplevel)/brew/install-package.sh" \
+	hyperfine hyperfine
