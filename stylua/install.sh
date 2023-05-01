140f7a7cef449e87a13e039edee5ea333ae4eb69
Derek Wan <d.wan@icloud.com>
2023-05-01 15:47:08 +0800

    most

--- stylua/install.sh
+++ stylua/install.sh
@@ -1,36 +1,15 @@
 #!/usr/bin/env bash
 
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking for stylua..."
+echo "$(date '+%Y-%m-%d %H:%M:%S'): Running stylua/install.sh..."
 
 if ! [ -x "$(command -v stylua)" ]; then
-	echo "$(date '+%Y-%m-%d %H:%M:%S'): Installing stylua..."
-	_root="$(git rev-parse --show-toplevel)"
-	case "$(uname -s)" in
-	Darwin*)
-		# shellcheck source=/dev/null
-		source "$_root/brew/install-package.sh" stylua stylua
-		;;
-	Linux*)
-		_temp_dir="$(mktemp -d -t sd-"$(date +%Y%m%d-%H%M%S)"-XXXXXXXX)"
-		# shellcheck source=/dev/null
-		source "$_root/installers/download-from-github.sh" \
-			"$_temp_dir" JohnnyMorganz StyLua linux-x86_64
-		(
-			cd "$_temp_dir" || exit
-			unzip ./*.zip
-			chmod u+x stylua
-			_bin_dir="$HOME/.local/bin/"
-			mkdir -p "$_bin_dir"
-			mv stylua "$_bin_dir/"
-		)
-		rm -rf "$_temp_dir"
-		;;
-	*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
-	esac
+	# shellcheck source=/dev/null
+	source "$(git rev-parse --show-toplevel)/brew/install.sh"
+	brew install stylua
 fi
 
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Symlinking for stylua..."
+# symlinks
 # shellcheck source=/dev/null
-source "$_root/installers/symlink.sh" \
+source "$(git rev-parse --show-toplevel)/installers/symlink.sh" \
 	"$HOME/dotfiles/stylua/stylua.toml" \
 	"${XDG_CONFIG_HOME:-$HOME/.config}/stylua/stylua"
