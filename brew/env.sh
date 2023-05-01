885b00ac64343c1bff64ec1b9b51d0ae8b645cdb
Derek Wan <d.wan@icloud.com>
2023-05-01 14:04:18 +0800

    fix brew env

--- brew/env.sh
+++ brew/env.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-if [ -x "$(command -v brew)" ]; then
-	_bin="$(brew --prefix)/bin"
+case "$(uname -s)" in
+Darwin*)
+	_bin=/opt/homebrew/bin
 	if [ -d "$_bin" ]; then
 		export PATH="$_bin${PATH:+:$PATH}"
 	fi
-fi
+	;;
+Linux*)
+	_bin=/home/linuxbrew/.linuxbrew/bin
+	if [ -d "$_bin" ]; then
+		export PATH="$_bin${PATH:+:$PATH}"
+	fi
+	;;
+*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
+esac
