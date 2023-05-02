39625d0d86f7d4b194c40bf7c9d5a1f0fad0968e
Derek Wan <d.wan@icloud.com>
2023-05-02 19:26:37 +0800

    Add `net-tools` for Linux

---
+++ net-tools/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "$(date '+%Y-%m-%d %H:%M:%S'): Running neovim/install.sh..."
+
+if [[ "$(uname -s)" =~ Linux* ]]; then
+	# shellcheck source=/dev/null
+	source "$(git rev-parse --show-toplevel)/brew/install.sh"
+	if ! grep -Fxq net-stat <<<"$(brew list -1)"; then
+		brew install net-stat
+	fi
+fi
