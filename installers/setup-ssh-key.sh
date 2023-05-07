70fddd2b6356b6f355b28d1f4bb745f91b836539
Derek Wan <d.wan@icloud.com>
2023-05-07 09:08:01 +0800

    Hidden vars in `setup-ssh-key`

--- installers/setup-ssh-key.sh
+++ installers/setup-ssh-key.sh
@@ -2,29 +2,29 @@
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Checking SSH key..."
 
-path_ssh_key="$HOME/.ssh/id_rsa"
-root="$(git rev-parse --show-toplevel)"
-if ! [ -f "$path_ssh_key" ]; then
+_path_ssh_key="$HOME/.ssh/id_rsa"
+_root="$(git rev-parse --show-toplevel)"
+if ! [ -f "$_path_ssh_key" ]; then
 	echo "$(date '+%Y-%m-%d %H:%M:%S'): Setting up SSH key... "
 	echo "Please copy the private key for this machine ($HOSTNAME)"
 	echo "You may wish to sign in at: https://my.1password.com/signin"
 	echo "Press 'y' or 'n' to continue..."
-	old_stty_cfg=$(stty -g)
+	_old_stty_cfg=$(stty -g)
 	stty raw -echo
-	answer=$(while ! head -c 1 | grep -i '[yn]'; do true; done)
-	stty "$old_stty_cfg"
-	if [ "$answer" != "${answer#[Yy]}" ]; then
-		mkdir -p "$(dirname "$path_ssh_key")"
+	_answer=$(while ! head -c 1 | grep -i '[yn]'; do true; done)
+	stty "$_old_stty_cfg"
+	if [ "$_answer" != "${_answer#[Yy]}" ]; then
+		mkdir -p "$(dirname "$_path_ssh_key")"
 		case "$(uname -s)" in
-		Darwin*) pbpaste >>"$path_ssh_key" ;;
+		Darwin*) pbpaste >>"$_path_ssh_key" ;;
 		Linux*)
 			# shellcheck source=/dev/null
-			source "$root/xsel/install.sh"
-			xsel -b >>"$path_ssh_key"
+			source "$_root/xsel/install.sh"
+			xsel -b >>"$_path_ssh_key"
 			;;
 		*) echo "$(date '+%Y-%m-%d %H:%M:%S'): Invalid OS: $(uname -s)..." ;;
 		esac
-		chmod g-rwx,o-rwx "$path_ssh_key"
+		chmod g-rwx,o-rwx "$_path_ssh_key"
 	else
 		echo "$(date '+%Y-%m-%d %H:%M:%S'): Exiting..."
 	fi
