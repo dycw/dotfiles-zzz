1e8ed32f768aab180e0778671c2bbe872b88b071
Derek Wan <d.wan@icloud.com>
2023-05-15 00:09:19 +0800

    Add `hyper` config

--- hyper/install.sh
+++ hyper/install.sh
@@ -3,9 +3,15 @@
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Running hyper/install.sh..."
 
 if [[ "$(uname -s)" =~ Darwin* ]]; then
+	_root="$(git rev-parse --show-toplevel)"
+
 	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/brew/install.sh"
+	source "$_root/brew/install.sh"
 	if ! grep -Fxq hyper <<<"$(brew list -1)"; then
 		brew install --cask hyper
 	fi
+
+	# shellcheck source=/dev/null
+	source "$_root/installers/symlink.sh" \
+		"$HOME/dotfiles/hyper/hyper.js" "$HOME/.hyper.js"
 fi
