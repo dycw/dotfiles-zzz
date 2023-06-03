99da767d58ea441f6e912e23500f333852d2a650
Derek Wan <d.wan@icloud.com>
2023-06-03 12:38:26 +0800

    Extend to `typescript` too (#373)

--- lunarvim/luasnippets/typescriptreact.lua
+++ lunarvim/luasnippets/typescriptreact.lua
@@ -1,12 +1,1 @@
--- luacheck: ignore 113
--- luacheck: ignore 142
-
-return {
-	s("edf", {
-		t("export default function "),
-		i(1),
-		t({ "() {", "  " }),
-		i(2),
-		t({ "", "}" }),
-	}),
-}
+typescript.lua
