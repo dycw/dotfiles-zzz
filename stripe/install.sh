16f7a87695c5d512093a2aade12c7d78ddf00c63
Derek Wan <d.wan@icloud.com>
2023-06-08 20:32:21 +0800

    Add `stripe` CLI and more snippets (#376)

---
+++ stripe/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "$(date '+%Y-%m-%d %H:%M:%S'): Running stripe/install.sh..."
+
+if ! [ -x "$(command -v stripe)" ]; then
+	# shellcheck source=/dev/null
+	source "$(git rev-parse --show-toplevel)/brew/install.sh"
+	brew install stripe/stripe-cli/stripe
+fi
