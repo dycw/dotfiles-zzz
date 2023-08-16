e5484b4f886fc1ec907c92ddd80cfa56ff60c4cf
Derek Wan <d.wan@icloud.com>
2023-08-16 22:14:51 +0900

    Remove `net-tools`, `sed` is Mac only

--- sed/install.sh
+++ sed/install.sh
@@ -2,7 +2,7 @@
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Running sed/install.sh..."
 
-if ! [ -x "$(command -v gsed)" ]; then
+if [[ "$(uname -s)" =~ Darwin* ]] && ! [ -x "$(command -v gsed)" ]; then
 	# shellcheck source=/dev/null
 	source "$(git rev-parse --show-toplevel)/brew/install.sh"
 	brew install gnu-sed
