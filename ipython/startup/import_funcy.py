68e8b304ba19d7917f2d74381ddcba75d6178717
Derek Wan <d.wan@icloud.com>
2023-09-05 06:20:05 +0800

    More snippets (#403)

--- ipython/startup/import_funcy.py
+++ ipython/startup/import_funcy.py
@@ -1,8 +1,9 @@
 from contextlib import suppress
 
-
 with suppress(ModuleNotFoundError):
     import funcy  # noqa: F401
-    from funcy import complement  # noqa: F401
-    from funcy import isnone  # noqa: F401
-    from funcy import notnone  # noqa: F401
+    from funcy import (
+        complement,  # noqa: F401
+        isnone,  # noqa: F401
+        notnone,  # noqa: F401
+    )
--- ipython/startup/import_holoviews.py
+++ ipython/startup/import_holoviews.py
@@ -1,12 +1,13 @@
 from contextlib import suppress
 
-
 with suppress(ModuleNotFoundError):
     import holoviews  # noqa: F401
     import holoviews as hv  # noqa: F401
-    from holoviews import Curve  # noqa: F401
-    from holoviews import Histogram  # noqa: F401
-    from holoviews import HLine  # noqa: F401
-    from holoviews import NdOverlay  # noqa: F401
-    from holoviews import Overlay  # noqa: F401
-    from holoviews import Table  # noqa: F401
+    from holoviews import (
+        Curve,  # noqa: F401
+        Histogram,  # noqa: F401
+        HLine,  # noqa: F401
+        NdOverlay,  # noqa: F401
+        Overlay,  # noqa: F401
+        Table,  # noqa: F401
+    )
