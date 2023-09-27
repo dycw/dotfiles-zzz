6cd92b51d676eb968431d4b8e4b511cd8ee2b7f7
Derek Wan <d.wan@icloud.com>
2023-09-27 22:59:09 +0800

    More snippets

--- ipython/startup/import_inspect.py
+++ ipython/startup/import_inspect.py
@@ -1,3 +1,2 @@
 import inspect  # noqa: F401
-from inspect import getattr_static  # noqa: F401
-from inspect import signature  # noqa: F401
+from inspect import getattr_static, signature  # noqa: F401  # noqa: F401
--- ipython/startup/import_matplotlib.py
+++ ipython/startup/import_matplotlib.py
@@ -1,6 +1,6 @@
 from contextlib import suppress
 
-
 with suppress(ModuleNotFoundError):
     import matplotlib  # noqa: F401
+    import matplotlib as mpl  # noqa: F401
     import matplotlib.pyplot as plt  # noqa: F401
