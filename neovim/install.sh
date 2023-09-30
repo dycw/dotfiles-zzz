de256696dcc1e7921f1f5f48526c45c163b332bb
Derek Wan <d.wan@icloud.com>
2023-10-01 07:53:58 +0800

    Add lazyvim

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,8 +2,15 @@
 
 echo "$(date '+%Y-%m-%d %H:%M:%S'): Running neovim/install.sh..."
 
+_root="$(git rev-parse --show-toplevel)"
 if ! [ -x "$(command -v nvim)" ]; then
 	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/brew/install.sh"
+	source "$_root/brew/install.sh"
 	brew install neovim
 fi
+
+# symlinks
+# shellcheck source=/dev/null
+source "$_root/installers/symlink.sh" \
+	"$HOME/dotfiles/neovim/lazyvim" \
+	"${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
