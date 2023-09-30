79afa8ec26133670100239347fa76f13ad7291bb
Derek Wan <d.wan@icloud.com>
2023-10-01 07:59:15 +0800

    Add lazyvim (#413)

---
+++ neovim/lazyvim/lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			fish = { "fish_indent" },
+			sh = { "shfmt" },
+			python = { "black" },
+		},
+	},
+}
