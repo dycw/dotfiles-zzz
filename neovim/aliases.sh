9d0a6052e2f9c2e5540ce7553c9774c969778d53
Derek Wan <d.wan@icloud.com>
2023-10-01 09:14:25 +0800

    More `lazyvim`, use fewer `pytest` aliases
    Drop n20 etc from aliases
    
    More plugins
    
    more
    
    asdf

--- neovim/aliases.sh
+++ neovim/aliases.sh
@@ -6,5 +6,5 @@ if [ -x "$(command -v nvim)" ] && ! [ -x "$(command -v lvim)" ]; then
 	alias keymapslua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/keymaps.lua"'
 	alias lazylua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/lazy.lua"'
 	alias optionslua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/options.lua"'
-	alias cdplugins='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua"'
+	alias cdplugins='cd "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/plugins"'
 fi
