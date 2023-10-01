33f51958c48adf69af1413b9ed8c6be0de1dfeac
Derek Wan <d.wan@icloud.com>
2023-10-01 08:37:02 +0800

    More `lazyvim` configuration (#415)

--- neovim/aliases.sh
+++ neovim/aliases.sh
@@ -2,4 +2,9 @@
 
 if [ -x "$(command -v nvim)" ] && ! [ -x "$(command -v lvim)" ]; then
 	alias n='nvim'
+	alias autocmdslua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/autocmds.lua"'
+	alias keymapslua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/keymaps.lua"'
+	alias lazylua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/lazy.lua"'
+	alias optionslua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/options.lua"'
+	alias cdplugins='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua"'
 fi
--- neovim/lazyvim/lua/config/keymaps.lua
+++ neovim/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,49 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+	local keys = require("lazy.core.handler").handlers.keys
+	---@cast keys LazyKeysHandler
+	-- do not create the keymap if a lazy keys handler exists
+	if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+		opts = opts or {}
+		opts.silent = opts.silent ~= false
+		if opts.remap and not vim.g.vscode then
+			opts.remap = nil
+		end
+		vim.keymap.set(mode, lhs, rhs, opts)
+	end
+end
+
+-------------------------------------------------------------------------------
+-- command mode
+map({ "n", "v" }, "<CR>", ":", { desc = "Command mode" })
+
+-- marks
+local prefixes = "m'"
+local letters = "abcdefghijklmnopqrstuvwxyz"
+for i = 1, #prefixes do
+	local prefix = prefixes:sub(i, i)
+	for j = 1, #letters do
+		local lower_letter = letters:sub(j, j)
+		local upper_letter = string.upper(lower_letter)
+		map({ "n", "v" }, prefix .. lower_letter, prefix .. upper_letter, { desc = "Mark " .. upper_letter })
+	end
+end
+
+-- navigation
+map("i", "<C-h>", "<Left>", { desc = "Left" })
+map("i", "<C-j>", "<Down>", { desc = "Down" })
+map("i", "<C-k>", "<Up>", { desc = "Up" })
+map("i", "<C-l>", "<Right>", { desc = "Right" })
+
+-- paste
+map({ "n", "v", "i" }, "<F2>", "<Cmd>set invpaste paste?<CR>", { desc = "Toggle paste mode" })
+
+-- quickfix
+map("n", "]", "<Cmd>cnext<CR>", { desc = "Next in list" })
+map("n", "[", "<Cmd>cprev<CR>", { desc = "Prev in list" })
+
+-- quit
+map("n", "<C-q>", "<Cmd>q<CR>", { desc = "Quit" })
