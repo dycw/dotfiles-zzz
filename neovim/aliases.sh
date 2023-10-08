f2fa78f9207dea06535e23ed0b7ac7a39ba30c33
Derek Wan <d.wan@icloud.com>
2023-10-08 18:28:34 +0800

    Set up luasnip

--- neovim/aliases.sh
+++ neovim/aliases.sh
@@ -7,4 +7,5 @@ if [ -x "$(command -v nvim)" ]; then
 	alias lazylua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/lazy.lua"'
 	alias optionslua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/config/options.lua"'
 	alias cdplugins='cd "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/plugins"'
+	alias luasniplua='$EDITOR "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/plugins/luasnip.lua"'
 fi
