b4491c3402560426c44f9b41a7313191d43124d5
Derek Wan <d.wan@icloud.com>
2023-10-08 12:46:47 +0800

    Add more key bindings, disable change notifications (#430)

--- neovim/lazyvim/lua/plugins/autosave.lua
+++ neovim/lazyvim/lua/plugins/autosave.lua
@@ -1,7 +1,9 @@
 return {
 	"okuuva/auto-save.nvim",
 	config = function()
-		vim.keymap.set("n", "<Leader>a", "<Cmd>ASToggle<CR>", { noremap = true })
 		require("auto-save").setup()
 	end,
+	keys = {
+		{ "<Leader>a", "<Cmd>ASToggle<CR>", desc = "Toggle AutoSave" },
+	},
 }
--- neovim/lazyvim/lua/plugins/sort.lua
+++ neovim/lazyvim/lua/plugins/sort.lua
@@ -1,3 +1,7 @@
 return {
 	"sqve/sort.nvim",
+	keys = {
+		{ "<Leader>so", "<Cmd>Sort<CR>", desc = "Sort", mode = { "n", "v" } },
+		{ "<Leader>sn", "<Cmd>Sort n<CR>", desc = "S numbersort", mode = { "n", "v" } },
+	},
 }
