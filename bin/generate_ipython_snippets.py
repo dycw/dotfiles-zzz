ffc3921fa742ccd203437f4c99bc75e2a826335c
Derek Wan <d.wan@icloud.com>
2023-11-09 22:26:53 +0800

    Add snippets (#461)

--- bin/generate_ipython_snippets.py
+++ bin/generate_ipython_snippets.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
-import abc as module
 from re import search
 from sys import stdout
+
+import utilities.typing as module
 
 for attr in dir(module):
     if not search(r"^_", attr):
