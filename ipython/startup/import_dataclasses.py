97fb370475c0d96a20982f31b5e356150711681f
Derek Wan <d.wan@icloud.com>
2023-11-11 19:17:28 +0800

    Add snippets

--- ipython/startup/import_dataclasses.py
+++ ipython/startup/import_dataclasses.py
@@ -2,9 +2,16 @@ from __future__ import annotations
 
 import dataclasses  # noqa: F401
 from dataclasses import (
+    KW_ONLY,  # noqa: F401
+    MISSING,  # noqa: F401
+    Field,  # noqa: F401
+    FrozenInstanceError,  # noqa: F401
     dataclass,  # noqa: F401
+    is_dataclass,
+    make_dataclass,  # noqa: F401
     replace,  # noqa: F401
 )
+from typing import Any
 
 try:
     import attrs  # noqa: F401
@@ -15,3 +22,7 @@ except ModuleNotFoundError:
         field,  # noqa: F401
         fields,  # noqa: F401
     )
+
+
+def is_dataclass_instance(obj: Any, /) -> bool:
+    return (not isinstance(obj, type)) and is_dataclass(obj)
--- ipython/startup/import_gzip.py
+++ ipython/startup/import_gzip.py
@@ -1,3 +1,9 @@
 from __future__ import annotations
 
 import gzip  # noqa: F401
+from gzip import (
+    BadGzipFile,  # noqa: F401
+    GzipFile,  # noqa: F401
+    compress,  # noqa: F401
+    decompress,  # noqa: F401
+)
