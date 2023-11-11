42ce9f05bc0e5125e4487a255fa1f24040428abf
Derek Wan <d.wan@icloud.com>
2023-11-11 13:06:39 +0800

    Remove `black` (#463)

--- black/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Running black/install.sh..."
-
-if ! [ -x "$(command -v black)" ]; then
-	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/brew/install.sh"
-	brew install black
-fi
