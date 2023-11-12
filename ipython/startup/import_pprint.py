ebc9312dd94a03bde4277f59ac087d210125f708
Derek Wan <d.wan@icloud.com>
2023-11-12 15:27:09 +0800

    More snippets

---
+++ ipython/startup/import_pprint.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from pprint import (
+    PrettyPrinter,  # noqa: F401
+    isreadable,  # noqa: F401
+    isrecursive,  # noqa: F401
+    pformat,  # noqa: F401
+    pp,  # noqa: F401
+    pprint,  # noqa: F401
+    saferepr,  # noqa: F401
+)
