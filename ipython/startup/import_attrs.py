c9bc4c3b924480fdcef54f4c5c66a6562ed664d7
Derek Wan <d.wan@icloud.com>
2023-11-28 20:57:56 +0800

    Update snippets

--- ipython/startup/import_attrs.py
+++ ipython/startup/import_attrs.py
@@ -5,11 +5,7 @@ from contextlib import suppress
 with suppress(ModuleNotFoundError):
     import attrs  # noqa: F401
     from attrs import (
-        asdict,  # noqa: F401
-        astuple,  # noqa: F401
         define,  # noqa: F401
         evolve,  # noqa: F401
-        field,  # noqa: F401
-        fields,  # noqa: F401
         frozen,  # noqa: F401
     )
