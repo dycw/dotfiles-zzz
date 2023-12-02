47cc8c5378a31f6301b535961ee20e78d8d008a9
Derek Wan <d.wan@icloud.com>
2023-12-02 14:34:27 +0800

    Add `cvxpy` imports (#493)

--- ipython/startup/import_cvxpy.py
+++ ipython/startup/import_cvxpy.py
@@ -4,3 +4,10 @@ from contextlib import suppress
 
 with suppress(ModuleNotFoundError):
     import cvxpy  # noqa: F401
+    from cxvpy import (
+        Expression,  # noqa: F401
+        Maximize,  # noqa: F401
+        Minimize,  # noqa: F401
+        Problem,  # noqa: F401
+        Variable,  # noqa: F401
+    )
