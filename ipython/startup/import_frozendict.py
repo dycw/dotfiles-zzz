b884b56c2a1e5a58bdf70fd96f5201f59e87d528
Derek Wan <d.wan@icloud.com>
2023-12-04 21:33:11 +0800

    More snippets (#496)

--- ipython/startup/import_frozendict.py
+++ ipython/startup/import_frozendict.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from contextlib import suppress
 
 with suppress(ModuleNotFoundError):
-    import xlsxwriter  # noqa: F401
-    from xlsxwriter import (
-        Workbook,  # noqa: F401
+    from frozendict import (
+        frozendict,  # noqa: F401
     )
