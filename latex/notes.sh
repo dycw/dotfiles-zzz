a1372348c4e9881a5adad9207b40833a17650684
Derek Wan <d.wan@icloud.com>
2024-01-09 23:26:16 +0800

    Add `pandoc`, and notes on `latex` (#509)

---
+++ latex/notes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo tlmgr install tcolorbox
+sudo tlmgr install environ
+sudo tlmgr install pdfcol
+sudo tlmgr install adjustbox
+sudo tlmgr install titling
+sudo tlmgr install enumitem
+sudo tlmgr install soul
+
+sudo tlmgr install collection-fontsrecommended
