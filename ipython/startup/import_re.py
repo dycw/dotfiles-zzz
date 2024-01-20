d031132c35234321be50e6d9a515e30b8e6e760f
Derek Wan <d.wan@icloud.com>
2024-01-20 11:28:09 +0800

    Add snippets

--- ipython/startup/import_re.py
+++ ipython/startup/import_re.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 import re  # noqa: F401
 from re import (
+    DOTALL,  # noqa: F401
     escape,  # noqa: F401
     findall,  # noqa: F401
     fullmatch,  # noqa: F401
     match,  # noqa: F401
+    search,  # noqa: F401
 )
