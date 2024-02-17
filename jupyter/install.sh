ed8754312a51364eacb20b837ec461cc19566a5a
Derek Wan <d.wan@icloud.com>
2024-02-17 16:18:48 +0800

    Use `platform_dirs` for `jupyter`

--- jupyter/install.sh
+++ jupyter/install.sh
@@ -7,10 +7,15 @@ _items=(
 	jupyter_server_config.py:jupyter_server_config.py
 	user-settings:lab/user-settings
 )
+if [[ "$(uname -s)" =~ Darwin* ]]; then
+	_ROOT="$HOME/Library/Application Support/Jupyter"
+elif [[ "$(uname -s)" =~ Linux* ]]; then
+	_ROOT="$HOME/.config/Jupyter"
+fi
 for _item in "${_items[@]}"; do
 	_key="$(echo "$_item" | cut -d ':' -f 1)"
 	_value="$(echo "$_item" | cut -d ':' -f 2)"
 	# shellcheck source=/dev/null
 	source "$(git rev-parse --show-toplevel)/installers/symlink.sh" \
-		"$HOME/dotfiles/jupyter/$_key" "$HOME/.jupyter/$_value"
+		"$HOME/dotfiles/jupyter/$_key" "$_ROOT/$_value"
 done
