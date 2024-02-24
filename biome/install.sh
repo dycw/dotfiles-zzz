f7ff5dfb8a7ee424e09a437cc2121f9f6d8f5dc5
Derek Wan <d.wan@icloud.com>
2024-02-24 17:58:11 +0800

    more

--- biome/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Running biome/install.sh..."
-
-if ! [ -x "$(command -v biome)" ]; then
-	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/brew/install.sh"
-	brew install biome
-fi
--- stripe/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Running stripe/install.sh..."
-
-if [[ "$(uname -s)" =~ Darwin* ]] && ! [ -x "$(command -v stripe)" ]; then
-	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/brew/install.sh"
-	brew install stripe/stripe-cli/stripe
-fi
