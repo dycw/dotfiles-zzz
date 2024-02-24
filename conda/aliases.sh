fd481d6192206c052da71e70f061645e18650158
Derek Wan <d.wan@icloud.com>
2024-02-24 17:53:46 +0800

    remove

---
+++ conda/aliases.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+alias conda-d='conda deactivate'
--- tesseract/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Running tesseract/install.sh..."
-
-if ! [ -x "$(command -v tesseract)" ]; then
-	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/brew/install.sh"
-	brew install tesseract
-fi
