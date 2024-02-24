833432a2e746664711e59a494d4a70e15be748b5
Derek Wan <d.wan@icloud.com>
2024-02-24 18:03:09 +0800

    more

--- netlify/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Running netlify/install.sh..."
-
-if ! [ -x "$(command -v netlify)" ]; then
-	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/npm/install.sh"
-	npm install -g netlify-cli
-fi
--- prisma/aliases.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -x "$(command -v pnpx)" ]; then
-	alias prig='pnpx prisma generate'
-	alias primd='pnpx prisma migrate dev'
-elif [ -x "$(command -v npx)" ]; then
-	alias prig='npx prisma generate'
-	alias primd='npx prisma migrate dev'
-fi
