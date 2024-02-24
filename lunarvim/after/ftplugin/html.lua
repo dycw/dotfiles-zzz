96896e88a9ce57b02295495054dbebfeb3e3df3a
Derek Wan <d.wan@icloud.com>
2024-02-24 18:00:58 +0800

    more

--- lunarvim/after/ftplugin/html.lua
+++
@@ -1,7 +0,0 @@
--- luacheck: ignore 112
-
-vim.api.nvim_buf_set_keymap(0, "n", "<Leader>bs", "<Cmd>Bracey<CR>", { noremap = true, silent = true })
-vim.api.nvim_buf_set_keymap(0, "n", "<Leader>br", "<Cmd>BraceyReload<CR>", { noremap = true, silent = true })
-
-require("lvim.lsp.manager").setup("emmet_ls")
-require("lvim.lsp.manager").setup("tailwindcss")
