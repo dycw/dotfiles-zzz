1e2789178f13b2ebf778469d20ac7d974e5721e8
Derek Wan <d.wan@icloud.com>
2024-02-24 17:59:21 +0800

    more

--- eslint/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Running eslint/install.sh..."
-
-if ! grep -Fxq eslint <<<"$(brew list -1)"; then
-	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/brew/install.sh"
-	brew install eslint
-fi
--- hyperfine/aliases.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -x "$(command -v hyperfine)" ]; then
-	alias time='hyperfine'
-fi
