61677830cbe06fe19fa73aa8f31cc78b4cd2b024
Derek Wan <d.wan@icloud.com>
2024-02-25 10:39:25 +0800

    Add more aliases & snippets (#540)
    * Add more aliases
    
    * Add more

--- uv/aliases.sh
+++ uv/aliases.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -x "$(command -v uv)" ]; then
+	alias uvpi='uv pip install'
 	alias uvpie='uv pip install --editable .'
 	alias uvps='uv pip sync --strict requirements.txt'
 fi
