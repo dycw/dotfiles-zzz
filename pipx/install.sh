82fe96ea42e36750fb25445be17bd5b255ad7533
Derek Wan <d.wan@icloud.com>
2024-03-05 23:42:13 +0800

    Remove `pipx` (#550)

--- pipx/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(date '+%Y-%m-%d %H:%M:%S'): Running pipx/install.sh..."
-
-if ! [ -x "$(command -v pipx)" ]; then
-	# shellcheck source=/dev/null
-	source "$(git rev-parse --show-toplevel)/brew/install.sh"
-	brew install pipx
-fi
