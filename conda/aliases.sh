c58fbe129286abb5e1fe747c142b248feb6f4d52
Derek Wan <d.wan@icloud.com>
2024-03-06 21:33:40 +0800

    Add `conda-a`

--- conda/aliases.sh
+++ conda/aliases.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+function conda-a() {
+	env_name="$(grep '^name:' environment.yml | awk '{print $2}')"
+	if [ -n "$env_name" ]; then
+		eval "$(conda shell.bash hook)"
+		conda activate "$env_name"
+	fi
+}
+
 alias conda-d='conda deactivate'
